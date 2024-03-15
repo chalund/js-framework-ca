4e23c4e6a7b2b7c22f7068b42fbcbc9f1faae68e
chalund <114383740+chalund@users.noreply.github.com>
2024-03-15 15:43:05 +0100

    update Header with searchbar, navlinks & cart

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -11,7 +11,7 @@ function Searchbar() {
     };
 
   return (
-    <div className='flex py-3 px-6 border-b justify-between'>
+    <div className='flex px-10 justify-between'>
       <form action="" className="w-full max-w-md relative">
         <div className="relative flex items-center text-gray-400 focus-within:text-gray-600">
           <IoIosSearch className="w-5 h-5 absolute ml-3  pointer-events-none"/>
@@ -20,10 +20,10 @@ function Searchbar() {
             placeholder="Search"
             autoComplete="off"
             aria-label="Search"
-            className='pr-3 pl-10 font-semibold placeholder-gray-500 text-black rounded-2xl border-none ring-gray-300 focus:ring-gray-500 focus:ring-2'
+            className='pr-3 pl-10 py-2 font-semibold text-sm placeholder-gray-500 text-black rounded-2xl border-none ring-gray-300 focus:ring-gray-500 focus:ring-2'
           />
             <IoIosClose 
-            className="absolute top-1/2 transform -translate-y-1/2 right-28 cursor-pointer"
+            className="absolute top-1/2 transform -translate-y-1/2 right-3 w-5 h-5 cursor-pointer"
             onClick={handleCloseButton} 
           />
         </div>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../Nav'
+import Logo from '../Logo'
 
 
 
 export const Header = () => {
   return (
-    <div className='flex flex-col'>
+    <div className='bg-orange-300 sticky top-0 z-[20]'>
+      <div className="mx-auto max-w-screen-lg px-4 py-2 flex w-full items-center justify-between flex-wrap">
+        <Logo />
         <Navbar />
+      </div>
     </div>
   )
 }
