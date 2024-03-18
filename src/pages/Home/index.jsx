ba23a0c2d51471a20348d67174ad0b8d6f52e743
chalund <114383740+chalund@users.noreply.github.com>
2024-03-18 18:17:16 +0100

    adds styling to ProductList + discount function

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
+import { ProductList } from "../../components/ProductList"
 import { Search } from "../../components/Search"
 
 const Home = () => {
   return (
     <div className="mx-auto max-w-screen-xl">
       <h1>Home</h1>
-      <Search />
+      <ProductList />
+      {/* <Search /> */}
     </div>
   )
 }
