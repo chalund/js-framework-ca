266a989a811766c9dcda6f2f52bed92a8c75d7b7
chalund <114383740+chalund@users.noreply.github.com>
2024-03-19 10:44:22 +0100

    adds search bar to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Nav'
 import Logo from '../Logo'
+import { Search } from '../Search'
 
 
 
@@ -9,6 +10,7 @@ export const Header = () => {
     <div className='bg-orange-300 sticky top-0 z-[20]'>
       <div className="mx-auto max-w-screen-lg px-4 py-2 flex w-full items-center justify-between flex-wrap">
         <Logo />
+        <Search />
         <Navbar />
       </div>
     </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,6 @@ const Home = () => {
   return (
     <div className="mx-auto max-w-screen-xl">
       <h1>Home</h1>
-      <ProductList />
-      {/* <Search /> */}
     </div>
   )
 }
