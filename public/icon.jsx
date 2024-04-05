579f864b29e67543542dd137e0726af8f2eb12e4
chalund <114383740+chalund@users.noreply.github.com>
2024-04-05 18:55:12 +0200

    update icon

--- public/icon.jsx
+++ public/icon.jsx
@@ -1,11 +1,11 @@
-import { FcProcess } from "react-icons/fc";
+// import { FcProcess } from "react-icons/fc";
 
-const Favicon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-      <path d={FcProcess} />
-    </svg>
-  );
-};
+// const Favicon = () => {
+//   return (
+//     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+//       <path d={FcProcess} />
+//     </svg>
+//   );
+// };
 
-export default Favicon;
+// export default Favicon;
