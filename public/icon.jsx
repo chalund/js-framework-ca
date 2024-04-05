ee116e0bcd467774f371d877d8623b4c94620113
chalund <114383740+chalund@users.noreply.github.com>
2024-04-05 18:49:49 +0200

    update size on icon

--- public/icon.jsx
+++ public/icon.jsx
@@ -2,7 +2,7 @@ import { FcProcess } from "react-icons/fc";
 
 const Favicon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
       <path d={FcProcess} />
     </svg>
   );
